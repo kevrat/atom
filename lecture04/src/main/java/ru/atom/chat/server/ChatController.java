33886b6f205dc8d474684cc11f7b22978db29606
kevrat <kevrat14@gmail.com>
2019-03-23 16:53:30 +0300

    Test success

--- lecture04/src/main/java/ru/atom/chat/server/ChatController.java
+++ lecture04/src/main/java/ru/atom/chat/server/ChatController.java
@@ -59,16 +59,56 @@ public class ChatController {
     /**
      * curl -X POST -i localhost:8080/chat/logout -d "name=I_AM_STUPID"
      */
-    //TODO
+    @RequestMapping(
+        path = "logout",
+        method = RequestMethod.POST,
+        consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> logout(@RequestParam("name") String name) {
+        if (!usersOnline.containsKey(name)) {
+            return ResponseEntity.badRequest().body("User '" + name  + "' was not logged in.");
+        }
+        usersOnline.remove(name);
+        messages.add("[" + name + "] logged out");
+        return ResponseEntity.ok().build();
+    }
 
     /**
      * curl -X POST -i localhost:8080/chat/say -d "name=I_AM_STUPID&msg=Hello everyone in this chat"
      */
-    //TODO
-
+    @RequestMapping(
+        path = "say",
+        method = RequestMethod.POST,
+        consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> say(@RequestParam("name") String name, @RequestParam("msg") String msg) {
+        if (!usersOnline.containsKey(name)) {
+            return ResponseEntity.badRequest().body("User '" + name  + "' was not logged in.");
+        }
+        messages.add("[" + name + "]: " + msg);
+        return ResponseEntity.ok().build();
+    }
 
     /**
      * curl -i localhost:8080/chat/chat
      */
-    //TODO
+    @RequestMapping(
+        path = "chat",
+        method = RequestMethod.GET,
+        produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> say() {
+        String responseBody = String.join("\n", messages);
+        return ResponseEntity.ok(responseBody);
+    }
+
+    @RequestMapping(
+        path = "clear",
+        method = RequestMethod.DELETE,
+        produces = MediaType.TEXT_PLAIN_VALUE
+    )
+    public ResponseEntity clear() {
+        messages.clear();
+        return ResponseEntity.noContent().build();
+    }
 }
